560daf8da7178da2b726e23402f3f31ada0075b9
Scott McAllister <stmcallister@gmail.com>
2017-01-21 23:16:53 -0800

    committing to github

---
+++ README.md
@@ -0,0 +1,17 @@
+# Smartsheet Increment Year
+Node.js sample for updating date values in Smartsheet
+
+This is a sample shows the following using the (smartsheet-javascript-sdk)[]:
+
+* Get Sheet
+* Update Row
+
+## Scenario
+I have a sheet where I keep track of a list of birthdays. An email notification is set up to remind me of these birthdays as they approach each year. 
+This is great, except for the list is getting long, and each year I need to update the year on each date. This script - in implimented as a scheduled cron job - automates that date increment process.
+
+## Setup
+* Access Token
+* Sheet ID
+
+
---
+++ smartsheet-increment-year.js
@@ -0,0 +1,67 @@
+var client = require('Smartsheet');
+var token = "your_token_here"; 
+var smartsheet = client.createClient({ accessToken: token });
+var sheetId = "your_sheet_id_here"; // your sheetId goes here
+var dateColumnNames = ["Birthday"];
+var dateColumnIds = [];
+var rowsToUpdate = [];
+
+function incrementYear(sheetId) {
+    // get sheet
+    smartsheet.sheets.getSheet({ id: sheetId }).then(function(sheet) {
+        // for each column title listed in the dateColumnNames array, get the columnId for that column and add it to the dateColumnIds array.
+    	for (var y = 0; y < dateColumnNames.length; y++) {
+	    	for (var i = 0; i < sheet.columns.length; i++) {
+	    		if (sheet.columns[i].title == dateColumnNames[y]) {
+	    			dateColumnIds.push(sheet.columns[i].id);
+	    		}
+	    	}
+		}
+        // loop the rows in the sheet
+		for (var r = 0; r < sheet.rows.length; r++) {
+            var row = {
+                    "id": sheet.rows[r].id,
+                    "cells": []
+                };
+            // loop the cells in the row
+			for (var c = 0; c < sheet.rows[r].cells.length; c++) {
+                for (var x = 0; x < dateColumnIds.length; x++) {
+					// for each cell with a columnId that matches a dateColumnId check the value of the date
+                    if (sheet.rows[r].cells[c].columnId == dateColumnIds[x]) {
+						var columnValueDate = new Date(sheet.rows[r].cells[c].value);
+                        // if the date value is in the past, increment the year    
+						if (columnValueDate.getFullYear() < new Date().getFullYear()) {
+                            // set the value to new year
+                            var newDate = new Date(columnValueDate.setFullYear(columnValueDate.getFullYear() + 1));
+                            // create a cell object for updating the sheet
+                            var cell = {
+                                "columnId": sheet.rows[r].cells[c].columnId,
+                                "value": newDate
+                            };
+                            // add the cell object to updated row object
+                            row.cells.push(cell);
+						}
+					}
+				}
+			}
+            // if the row contains cells add the row to the rowsToUpdate array
+            if (row.cells.length > 0) {
+                rowsToUpdate.push(row);
+            }
+		}
+        // build options object to send as part of the update request for smartsheet
+        var options = {
+            "sheetId": sheetId,
+            "body": rowsToUpdate
+        };
+        // send updateRow request to Smartsheet
+       smartsheet.sheets.updateRow(options).then(function (data) {
+           console.log(data);
+       })
+       .catch(function (error) {
+           console.log(error);
+       });
+    });
+}
+// execute script
+incrementYear(sheetId);
