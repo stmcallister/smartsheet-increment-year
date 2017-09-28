787a94405bef9934fd49d488dfcdf2fb9f4d2f93
Jorge Contreras <jorgeacontrerasl@gmail.com>
2017-09-28 15:38:31 -0700

    Add test file to work with sheet

--- smartsheet-increment-year-function-webtask.js
+++ smartsheet-increment-year-function-webtask.js
@@ -1,3 +1,7 @@
+const getDateColumn = (sheet, dateName) => {
+  return sheet.columns.find((column) => column.title === dateName)
+}
+
 module.exports = function (ctx, cb) {
     const client = require('smartsheet');
     const token = "your_token_here";
@@ -5,23 +9,23 @@ module.exports = function (ctx, cb) {
     const sheetId = "your_sheet_id_here";
     const dateColumnName = "Birthday"; // your date column you want incremented
     const weekAgo = new Date(new Date().setDate(new Date().getDate()-7)); // increasing the buffer to a week to account for differing dates and times across systems
-    
+
       // get sheet
       smartsheet.sheets.getSheet({ id: sheetId }).then(function(sheet) {
           // for each column title listed in the dateColumnNames array, get the columnId for that column and add it to the dateColumnIds array.
-          const dateColumn = sheet.columns.filter((column) => dateColumnName === column.title); 
-            
+          const dateColumn = sheet.columns.filter((column) => dateColumnName === column.title);
+
           const rowsToUpdate = sheet.rows.map((row) => {
-              return row = { 
-                  id: row.id, 
+              return row = {
+                  id: row.id,
                   cells: row.cells.
-                      filter((cell) => dateColumn[0].id === cell.columnId && new Date(cell.value) < weekAgo). 
+                      filter((cell) => dateColumn[0].id === cell.columnId && new Date(cell.value) < weekAgo).
                       map((cell) => {
                           var columnValueDate = new Date(cell.value);
                           cell.value = new Date(columnValueDate.setFullYear(columnValueDate.getFullYear() + 1))
                           return cell;
                       })
-              };      
+              };
           }).filter((row) => row.cells && row.cells.length > 0);
           // build options object to send as part of the update request for smartsheet
           var options = {
@@ -39,4 +43,3 @@ module.exports = function (ctx, cb) {
          });
       });
   };
-  
--- spec/test.js
+++ spec/test.js
@@ -1,5 +1,23 @@
-describe('calvin',function(){
-  it('likes hobbes', function(){
-    expect(true).toBe(false)
+const getDateColumn = (sheet, dateName) => {
+  return sheet.columns.find((column) => column.title === dateName).title
+}
+
+describe('getDateColumn',() => {
+  it('returns column name if argument meets column name', () => {
+    let sheet = {
+      columns: [
+        {
+          title: 'name',
+        },
+        {
+          title: 'related',
+        },
+        {
+          title: 'birthday',
+        },
+      ]
+    };
+    let result = getDateColumn(sheet,'birthday')
+    expect(result).toBe('birthday')
   })
 });
